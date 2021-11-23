ebf72976eaaef9d622f4b75ac2487504b9f9647f
Virthiya <virthiya.uthayanan98@gmail.com>
2021-11-23 15:06:42 -0500

    Modified AndroidManifest.xml - added back button functionality to activity_main_screen_owner.xml and activity_owner_add_product.xml

---
+++ app/src/main/java/com/example/quickbasket/OwnerAddProductPage.java
@@ -0,0 +1,15 @@
+package com.example.quickbasket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OwnerAddProductPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_add_product);
+    }
+
+}
