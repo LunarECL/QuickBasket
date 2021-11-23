dd25d7a121aeedac0b28b1463e13f6d322cb5b00
A2113S <shrivastava.ankit2113@gmail.com>
2021-11-23 16:29:59 +0530

    EDITED activity_main.xml to add a test button for Main Screen of Customer

--- app/src/main/java/com/example/quickbasket/MainActivity.java
+++ app/src/main/java/com/example/quickbasket/MainActivity.java
@@ -2,18 +2,42 @@ package com.example.quickbasket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //CODE FOR TEST MAIN CUSTOMER BUTTON
+        Button testButtonMainCustomer = findViewById(R.id.testMainCustomer);
+        testButtonMainCustomer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activity2Intent = new Intent(getApplicationContext(), main_screen_customer.class);
+                startActivity(activity2Intent);
+            }
+        });
     }
 
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, main_screen_customer.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 
     //sample Firebase Test code. It will be moved to customer register Activity soon.
     /*public void sendData(View view) {
