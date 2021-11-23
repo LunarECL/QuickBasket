8eee33403b0629d94eb98edf96868ffc79e62c65
A2113S <shrivastava.ankit2113@gmail.com>
2021-11-23 07:08:15 +0530

    Added java files for Customer Main Screen

--- app/src/main/java/com/example/quickbasket/MainActivity.java
+++ app/src/main/java/com/example/quickbasket/MainActivity.java
@@ -5,32 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActvity";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG,"onCreate:");
-        ListView mListView = (ListView) findViewById((R.id.listView));
-
-        //Create Person Objects
-        StoreInformation john = new StoreInformation("John", "12-20-1998", "Male");
-        StoreInformation steve = new StoreInformation("Steve", "08-03-1987", "Male");
-        StoreInformation stacy = new StoreInformation("Stacy", "11-15-2000", "Female");
-
-        ArrayList<StoreInformation> peopleList = new ArrayList<>();
-        peopleList.add(john);
-        peopleList.add(steve);
-        peopleList.add(stacy);
-
-        StoreInfoAdapter adapter = new StoreInfoAdapter(this, R.layout.customer_store_info_adapter_view_layout, peopleList);
-        mListView.setAdapter(adapter);
     }
 
 
