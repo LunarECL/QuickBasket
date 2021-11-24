da0e55489f8bca519bb178682cb91269845a21aa
Betroot02 <bealim138@gmail.com>
2021-11-24 15:46:50 +0400

    Edited MainActivity.java, activity_main.xml, AndroidManifest.xml

--- app/src/main/java/com/example/quickbasket/MainActivity.java
+++ app/src/main/java/com/example/quickbasket/MainActivity.java
@@ -16,6 +16,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // CODE FOR CUSTOMER LOGIN BUTTON
+        Button testButtonCustomerLogin = findViewById(R.id.customerLogin);
+        testButtonCustomerLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activity2Intent = new Intent(getApplicationContext(), LoginCustomerPage.class);
+                startActivity(activity2Intent);
+            }
+        });
+
+        // CODE FOR OWNER LOGIN BUTTON
+        Button testButtonOwnerLogin = findViewById(R.id.ownerLogin);
+        testButtonOwnerLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activity2Intent = new Intent(getApplicationContext(), LoginStoreOwnerPage.class);
+                startActivity(activity2Intent);
+            }
+        });
+
+        // CODE FOR CUSTOMER SIGNUP BUTTON
+        Button testButtonCustomerSignup = findViewById(R.id.customerSignup);
+        testButtonCustomerSignup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activity2Intent = new Intent(getApplicationContext(), SignupCustomerPage.class);
+                startActivity(activity2Intent);
+            }
+        });
+
+        // CODE FOR OWNER SIGNUP BUTTON
+        Button testButtonOwnerSignup = findViewById(R.id.ownerSignup);
+        testButtonOwnerSignup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activity2Intent = new Intent(getApplicationContext(), SignupOwnerPage.class);
+                startActivity(activity2Intent);
+            }
+        });
+
         //CODE FOR TEST MAIN CUSTOMER BUTTON
         Button testButtonMainCustomer = findViewById(R.id.testMainCustomer);
         testButtonMainCustomer.setOnClickListener(new View.OnClickListener() {
@@ -33,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(activity2Intent);
             }
         });
-
     }
 
 
