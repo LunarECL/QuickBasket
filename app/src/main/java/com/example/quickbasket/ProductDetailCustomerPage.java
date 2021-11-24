b252367e885780591abfde7195851a235350d987
LunarECL <38317983+LunarECL@users.noreply.github.com>
2021-11-24 20:45:32 +0900

    Added transection from StoreDetailCustomerPage to ProductDetailCustomerPage

--- app/src/main/java/com/example/quickbasket/ProductDetailCustomerPage.java
+++ app/src/main/java/com/example/quickbasket/ProductDetailCustomerPage.java
@@ -2,7 +2,9 @@ package com.example.quickbasket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProductDetailCustomerPage extends AppCompatActivity {
 
@@ -10,6 +12,19 @@ public class ProductDetailCustomerPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail_customer_page);
+
+        Intent intent = getIntent();
+        String sname = intent.getStringExtra("ID");
+
+
+        TextView t1 = (TextView) findViewById(R.id.StoreName);
+        t1.setText(sname);
+
+        TextView t2 = (TextView) findViewById(R.id.Description);
+        t2.setText("This is Description Sample just writing anything ~~~~~");
+
+        TextView t3 = (TextView) findViewById(R.id.Price);
+        t3.setText("$ 19.04");
     }
     //test
 }
