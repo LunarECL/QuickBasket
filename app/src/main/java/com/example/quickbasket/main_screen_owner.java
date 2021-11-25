a7741cc294f9514d64d46928792bfc6421d2a241
Virthiya <virthiya.uthayanan98@gmail.com>
2021-11-24 19:13:54 -0500

    Modified activity_main_screen_owner.xml - added back button (using activity_main_screen_customer.xml as reference)
    Modified main_screen_owner.java
    - added back button functionality (using main_screen_customer.java as reference)

--- app/src/main/java/com/example/quickbasket/main_screen_owner.java
+++ app/src/main/java/com/example/quickbasket/main_screen_owner.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class main_screen_owner extends AppCompatActivity {
 
@@ -12,6 +13,14 @@ public class main_screen_owner extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen_owner);
+
+        ImageButton backButton = findViewById(R.id.backButton_MainCustomer);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void onViewProductsClick(View view) {
