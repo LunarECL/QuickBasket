a6cdd0695f170be44cbb21eb83d89f341966ee63
Virthiya <virthiya.uthayanan98@gmail.com>
2021-11-27 14:33:08 -0500

    Modified main_screen_owner.java - added pseudocode for page functionality (correctness not guaranteed)
    Modified OwnerAddProductPage.java
    - added pseudocode for page functionality (correctness not guaranteed)

--- app/src/main/java/com/example/quickbasket/OwnerAddProductPage.java
+++ app/src/main/java/com/example/quickbasket/OwnerAddProductPage.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class OwnerAddProductPage extends AppCompatActivity {
+    Integer storeID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,42 @@ public class OwnerAddProductPage extends AppCompatActivity {
     }
 
     public void onAddProductClick(View view) {
+
+        // get storeID
+        /*DatabaseReference ref = FirebaseDatabase.getInstance().getReference("");
+        ref.child("").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("demo", "Error getting data", task.getException());
+                }
+                else {
+                    // Get store name
+                    Log.i("demo", task.getResult().getValue().toString());
+                    storeID = (Integer) task.getResult().getValue();
+                }
+            }
+        });*/
+
+        // Get values from text views
+        EditText nameEditText = (EditText) findViewById(R.id.editTextTextPersonName3);
+        EditText brandEditText = (EditText) findViewById(R.id.editTextTextPersonName6);
+        EditText priceEditText = (EditText) findViewById(R.id.editTextTextPersonName7);
+        EditText descriptionEditText = (EditText) findViewById(R.id.editTextTextMultiLine2);
+        EditText imageURLEditText = (EditText) findViewById(R.id.editTextTextPersonName10);
+        String name = nameEditText.getText().toString();
+        String brand = brandEditText.getText().toString();
+        String price_string = priceEditText.getText().toString();
+        Double price = Double.parseDouble(price_string);
+        String description = descriptionEditText.getText().toString();
+        String imageURL = imageURLEditText.getText().toString();
+
+        // Add product to DB
+        /*DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        Product product = new Product(storeID, name, description, brand, price, imageURL);
+        ref.child("").child("").setValue(product);*/
+
+        // Go to next screen
         Intent intent = new Intent(this, main_screen_owner.class);
         startActivity(intent);
     }
--- app/src/main/java/com/example/quickbasket/main_screen_owner.java
+++ app/src/main/java/com/example/quickbasket/main_screen_owner.java
@@ -8,12 +8,16 @@ import android.view.View;
 import android.widget.ImageButton;
 
 public class main_screen_owner extends AppCompatActivity {
+    String storeName;
+    String logo;
+    //ArrayList<Order> orders;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen_owner);
 
+        // Back Button
         ImageButton backButton = findViewById(R.id.backButton_StoreOwner);
         backButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -21,6 +25,67 @@ public class main_screen_owner extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Reading from DB to setup screen
+
+        // get store name
+        /*DatabaseReference ref = FirebaseDatabase.getInstance().getReference("");
+        ref.child("").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("demo", "Error getting data", task.getException());
+                }
+                else {
+                    // Get store name
+                    Log.i("demo", task.getResult().getValue().toString());
+                    storeName = (String) task.getResult().getValue();
+                }
+            }
+        });*/
+
+        // get logo
+        /*DatabaseReference ref = FirebaseDatabase.getInstance().getReference("");
+        ref.child("").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("demo", "Error getting data", task.getException());
+                }
+                else {
+                    // Get store name
+                    Log.i("demo", task.getResult().getValue().toString());
+                    logo = (String) task.getResult().getValue();
+                }
+            }
+        });*/
+
+        // get orders
+        /*DatabaseReference ref = FirebaseDatabase.getInstance().getReference("");
+        ref.child("").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("demo", "Error getting data", task.getException());
+                }
+                else {
+                    // Get store name
+                    Log.i("demo", task.getResult().getValue().toString());
+                    //orders = (ArrayList<Order>) task.getResult().getValue();
+                }
+            }
+        });*/
+
+
+        // Set store name to storeName
+        /*TextView storeNameTextView = (TextView) findViewById(R.id.textView12);
+        storeNameTextView.setText(storeName);*/
+
+        // Set logo to logo
+        // get image urland change image
+
+        // Setup orders
+        // get orders and display on screen
     }
 
     public void onViewProductsClick(View view) {
