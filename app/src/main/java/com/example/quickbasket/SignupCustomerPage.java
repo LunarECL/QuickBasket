0938e8c426a52cff2240496dfd12508f6f00b3f2
Betroot02 <bealim138@gmail.com>
2021-11-28 23:29:42 +0400

    Edited activity_signup_customer_page.xml, Customer.java, SignupCustomerPage.java, strings.xml, User.java

--- app/src/main/java/com/example/quickbasket/SignupCustomerPage.java
+++ app/src/main/java/com/example/quickbasket/SignupCustomerPage.java
@@ -5,16 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class SignupCustomerPage extends AppCompatActivity implements Contract.View{
+public class SignupCustomerPage extends AppCompatActivity{
 
-    private int counter = 0;
-    private Contract.Presenter presenter;
+
+    public void addCustomer(View view){
+        DatabaseReference db = FirebaseDatabase.getInstance().getReference();
+        EditText editUsername = (EditText) findViewById(R.id.enterUsername);
+        String username = editUsername.getText().toString();
+        EditText editPassword = (EditText) findViewById(R.id.enterPassword);
+        String password = editPassword.getText().toString();
+        EditText editName = (EditText) findViewById(R.id.enterName);
+        String name = editName.getText().toString();
+        db.child("userCount").setValue("0");
+        Customer customer = new Customer(db.child("userCount").getValue(), username, name, password);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,28 +37,19 @@ public class SignupCustomerPage extends AppCompatActivity implements Contract.Vi
             public void onClick(View v) {
                 Intent activity2Intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(activity2Intent);
-                //if (!usernameExists && !customeridExists && !passwordExists??)
-                counter++;
             }
         });
 
-        presenter = new MyPresenter(new MyModel(), this);
+        Button testButtonCustomerSignup = findViewById(R.id.signup_button);
+        testButtonCustomerSignup.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent activity2Intent = new Intent(getApplicationContext(), main_screen_customer.class);
+                startActivity(activity2Intent);
 
-    }
 
-    public String getUsername(){
-        EditText editUsername = (EditText) findViewById(R.id.enterUsername);
-        return editUsername.getText().toString();
-    }
+            }
+        });
 
-    public String getPassword(){
-        EditText editPassword = (EditText) findViewById(R.id.enterPassword);
-        return editPassword.getText().toString();
-    }
-
-    public void addCustomer(View view){
-        DatabaseReference signup = FirebaseDatabase.getInstance().getReference("Customers");
-        //Customer customer = new Customer(counter, this.getUsername(), )
     }
 
 }
