9a33861f5172e89c057f4ded933806f944c41e09
A2113S <shrivastava.ankit2113@gmail.com>
2021-11-29 01:07:16 +0530

    EDITED SignupCustomerPage

--- app/src/main/java/com/example/quickbasket/SignupCustomerPage.java
+++ app/src/main/java/com/example/quickbasket/SignupCustomerPage.java
@@ -1,14 +1,19 @@
 package com.example.quickbasket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -16,6 +21,7 @@ public class SignupCustomerPage extends AppCompatActivity{
 
 
     public void addCustomer(View view){
+
         DatabaseReference db = FirebaseDatabase.getInstance().getReference();
         EditText editUsername = (EditText) findViewById(R.id.enterUsername);
         String username = editUsername.getText().toString();
@@ -24,7 +30,20 @@ public class SignupCustomerPage extends AppCompatActivity{
         EditText editName = (EditText) findViewById(R.id.enterName);
         String name = editName.getText().toString();
         db.child("userCount").setValue("0");
-        Customer customer = new Customer(db.child("userCount").getValue(), username, name, password);
+
+        db.child("userCount").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                }
+            }
+        });
+
+        //Customer customer = new Customer(db.child("userCount").getValue(), username, name, password);
     }
 
     @Override
