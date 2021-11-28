131f79803325707fe0d6ae3ee32de5981018546a
adisan23 <sgacroit@liverpool.ac.uk>
2021-11-28 15:31:22 -0500

    Added setters and getters for Product.java and created ProductListAdapter

--- app/src/main/java/com/example/quickbasket/ViewOrderOwner.java
+++ app/src/main/java/com/example/quickbasket/ViewOrderOwner.java
@@ -1,5 +1,6 @@
 package com.example.quickbasket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,12 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -32,20 +39,43 @@ public class ViewOrderOwner extends AppCompatActivity {
         });
 
 
+        //Read from Database
+        /*
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Order");
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for(DataSnapshot child:snapshot.getChildren()){
+
+
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };*/
+
         //Demo Run with hardcoded items not relevant in future
 
-        Item item1 = new Item("iphone",800.0,"Apple");
-        Item item2 = new Item("surface pro",1000.0,"Microsoft");
-        Item item3 = new Item("pixel 6",700.0,"Google");
+        /*
+        Product product1 = new Product("iphone","phone","apple",200,"url");
+        Product product2 = new Product("surface pro","tablet","microsoft",600,"url");
 
-        ArrayList<Item> itemList = new ArrayList<>();
+        ArrayList<Product> productList = new ArrayList<>();
 
-        itemList.add(item1);
-        itemList.add(item2);
-        itemList.add(item3);
+        productList.add(product1);
+        productList.add(product2);
 
-        ItemListAdapter adapter = new ItemListAdapter(this, R.layout.adapter_view_layout, itemList);
+        ProductListAdapter adapter = new ProductListAdapter(this, R.layout.adapter_view_layout, productList);
+
         mListView.setAdapter(adapter);
+
+         */
 
 
 
