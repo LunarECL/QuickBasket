1033c9d8bbdb49977af7131dd9bd6f1cbad6e4b5
Betroot02 <bealim138@gmail.com>
2021-11-30 14:02:51 +0400

    Edited SignupCustomerPage.java

--- app/src/main/java/com/example/quickbasket/SignupCustomerPage.java
+++ app/src/main/java/com/example/quickbasket/SignupCustomerPage.java
@@ -31,72 +31,7 @@ public class SignupCustomerPage extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_customer_page);
 
-
-
-            DatabaseReference db = FirebaseDatabase.getInstance().getReference();
-            EditText editUsername = (EditText) findViewById(R.id.enterUsername);
-            String username = editUsername.getText().toString();
-            EditText editPassword = (EditText) findViewById(R.id.enterPassword);
-            String password = editPassword.getText().toString();
-            EditText editName = (EditText) findViewById(R.id.enterName);
-            String name = editName.getText().toString();
-
-
-            db.child("userCount").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<DataSnapshot> task) {
-                    if (!task.isSuccessful()) {
-                        Log.e("firebase", "Error getting data", task.getException());
-                    }
-                    else {
-                        if (task.getResult().getValue() == null){
-                            db.child("userCount").setValue(counter);
-                        }
-                        else{
-                            counter = Integer.parseInt(String.valueOf(task.getResult().getValue()));
-                        }
-                        Log.d("check outside", String.valueOf(counter));
-                    }
-                }
-            });
-
-            db.child("Customer").addListenerForSingleValueEvent(new ValueEventListener() {
-
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    Log.i("demo", "data changed");
-
-
-                    for (DataSnapshot dataSnapshot : snapshot.getChildren()){
-                        if (dataSnapshot.child("username").getValue().equals(username)){
-                            Toast.makeText(getApplicationContext(), "Username already exists. Please choose another username", Toast.LENGTH_SHORT).show();
-                            Log.d("Checking if statement", "check");
-                        }
-                        else{
-
-                            // increment counter
-                            counter += 1;
-                            // update the userCount
-                            db.child("userCount").setValue(counter);
-                            // create the customer object
-                            Customer customer = new Customer(counter , username, name, password);
-                            // set the customerID to customer object
-                            Log.d("Counter is this", String.valueOf(counter));
-                            db.child("Customer").child(String.valueOf(counter)).setValue(customer);
-                        }
-                    }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-
-                }
-            });
-
-            Intent intent = new Intent(this, MainScreenCustomer.class);
-            intent.putExtra("customerID", counter);
-            startActivity(intent);
-
+        // Back button to main screen
         ImageButton testBackButtonCustomerSignup = findViewById(R.id.backButton_CustomerSignup);
         testBackButtonCustomerSignup.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -104,11 +39,9 @@ public class SignupCustomerPage extends AppCompatActivity{
                 startActivity(activity2Intent);
             }
         });
-
     }
 
-
-    /*public void addCustomer(View view){
+    public void addCustomer(View view){
         DatabaseReference db = FirebaseDatabase.getInstance().getReference();
         EditText editUsername = (EditText) findViewById(R.id.enterUsername);
         String username = editUsername.getText().toString();
@@ -117,8 +50,8 @@ public class SignupCustomerPage extends AppCompatActivity{
         EditText editName = (EditText) findViewById(R.id.enterName);
         String name = editName.getText().toString();
 
-
-        db.child("userCount").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+        // Get the customer count
+        /*db.child("userCount").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
                 if (!task.isSuccessful()) {
@@ -134,7 +67,7 @@ public class SignupCustomerPage extends AppCompatActivity{
                     Log.d("check outside", String.valueOf(counter));
                 }
             }
-        });
+        });*/
 
         db.child("Customer").addListenerForSingleValueEvent(new ValueEventListener() {
 
@@ -142,23 +75,25 @@ public class SignupCustomerPage extends AppCompatActivity{
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 Log.i("demo", "data changed");
 
-
                 for (DataSnapshot dataSnapshot : snapshot.getChildren()){
-                    if (dataSnapshot.child("username").getValue().equals(username)){
+                    String dbUsername = String.valueOf(dataSnapshot.child("username").getValue());
+                    Log.d("Checking username", username);
+
+                    if (dbUsername.equals(username)){
                         Toast.makeText(getApplicationContext(), "Username already exists. Please choose another username", Toast.LENGTH_SHORT).show();
                         Log.d("Checking if statement", "check");
                     }
-                    else{
-
+                    else {
                         // increment counter
                         counter += 1;
                         // update the userCount
-                        db.child("userCount").setValue(counter);
+                        //db.child("userCount").setValue(counter);
                         // create the customer object
-                        Customer customer = new Customer(counter , username, name, password);
+                        Customer customer = new Customer(counter, username, name, password);
                         // set the customerID to customer object
                         Log.d("Counter is this", String.valueOf(counter));
                         db.child("Customer").child(String.valueOf(counter)).setValue(customer);
+                        break;
                     }
                 }
             }
@@ -173,8 +108,5 @@ public class SignupCustomerPage extends AppCompatActivity{
         intent.putExtra("customerID", counter);
         startActivity(intent);
 
-    }*/
-
-
-
+    }
 }
